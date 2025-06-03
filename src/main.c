2d1cb301d4c120c0d5e51352b66b836c81d926af
Pablo Trujillo <pabloctrujillov@gmail.com>
2025-06-03 23:03:47 +0800

    changes on main.c code to test ci

--- src/main.c
+++ src/main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
 int main() {
-    
+
     printf("Hello, World!\n");
     // Additional initialization and application logic can go here
+    printf("Antoher hello to check funcionallity.\n");
 
     return 0;
 }
